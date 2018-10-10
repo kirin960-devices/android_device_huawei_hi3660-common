80607e2649f53e52ea9dbe285738b8829b1bbde0
micky387 <mickaelsaibi@free.fr>
2018-10-10 20:57:16 +0200

    devicesettings: remove PROGUARD FLAG

--- devicesettings/Android.mk
+++ devicesettings/Android.mk
@@ -18,18 +18,13 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v7-preference \
     android-support-v7-recyclerview
 
-LOCAL_PROGUARD_FLAG_FILES := proguard.flags
-
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
     $(TOP)/packages/resources/devicesettings/res
 
 LOCAL_STATIC_JAVA_LIBRARIES := services.core.unboosted
 
-ifneq ($(INCREMENTAL_BUILDS),)
-    LOCAL_PROGUARD_ENABLED := disabled
-    LOCAL_JACK_ENABLED := incremental
-endif
+LOCAL_PROGUARD_ENABLED := disabled
 
 include frameworks/base/packages/SettingsLib/common.mk
 
